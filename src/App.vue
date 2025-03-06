48b62aaa2bc1b958bd3c8ccba261f9711b414518
MD RATON ISLAM <ratonislam127@gmail.com>
2025-03-06 16:52:12 +0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <Login />
+</div>
+</template>
+
+<script>
+import Login from './components/pages/login.vue';
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+
+</style>
